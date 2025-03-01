3382bd57b667df0537dea9b75ea0913bef7a68a9
kthorpe88 <kthorpe88@gmail.com>
2025-03-01 12:34:26 -0500

    Implement RGB effects for Caps Lock, Num Lock, and ESC key wi th ripple animation

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
 
-// T his program is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
@@ -15,71 +15,71 @@
 
 #pragma once
 
-//* Set 0 if debouncing isn't needed */
-	#define DEBOUNCE 3
-	
-	#define DYNAMIC_MACRO_EEPROM_STORAGE
-	#define DYNAMIC_MACRO_SIZE 128
+// Set 0 if debouncing isn't needed
+#define DEBOUNCE 3
+
+#define DYNAMIC_MACRO_EEPROM_STORAGE
+#define DYNAMIC_MACRO_SIZE 128
 
 #ifdef RGB_MATRIX_ENABLE
     #define RGB_MATRIX_LED_COUNT 104
     #define RGB_MATRIX_KEYRELEASES // reactive effects respond to keyreleases (instead of keypresses)
-	//#define RGB_MATRIX_TIMEOUT 0 // number of milliseconds to wait until rgb automatically turns off
-	//#define RGB_MATRIX_SLEEP // turn off effects when suspended
-	#define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-	#define RGB_MATRIX_LED_FLUSH_LIMIT 10 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-	#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
-	//#define RGB_MATRIX_DEFAULT_ON true // Sets the default enabled state, if none has been set
-	//#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT // Sets the default mode, if none has been set
-	//#define RGB_MATRIX_DEFAULT_HUE 0 // Sets the default hue value, if none has been set
-	//#define RGB_MATRIX_DEFAULT_SAT 255 // Sets the default saturation value, if none has been set
-	//#define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
-	#define RGB_MATRIX_DEFAULT_SPD 100 // Sets the default animation speed, if none has been set
-	#define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 50 //In order to change the delay of temperature decrease
-	#define RGB_MATRIX_TYPING_HEATMAP_SPREAD 40 //Limit the distance the effect spreads to surrounding keys.
-	#define RGB_MATRIX_TYPING_HEATMAP_AREA_LIMIT 16 //Limit how hot surrounding keys get from each press.
-	#define RGB_MATRIX_TYPING_HEATMAP_INCREASE_STEP 32 //Decreasing this value increases the number of keystrokes needed to fully heat up the key.
-	#define RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE //Solid reactive effects will pulse RGB light on key presses with user configurable hues. To enable gradient mode that will automatically change reactive color, add the following define:
-	#define ENALBE_RGB_MATRIX_SOLID_COLOR
-	#define ENABLE_RGB_MATRIX_ALPHAS_MODS
-	//#define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-	//#define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
-	#define ENABLE_RGB_MATRIX_BREATHING
-	//#define ENABLE_RGB_MATRIX_CYCLE_ALL
-	//#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-	//#define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
-	#define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-	//#define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
-	//#define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
-	#define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
-	#define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
-	#define ENABLE_RGB_MATRIX_DUAL_BEACON
-	#define ENABLE_RGB_MATRIX_RAINBOW_BEACON
-	#define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
-	#define ENABLE_RGB_MATRIX_RAINDROPS
-	#define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-	#define ENABLE_RGB_MATRIX_HUE_BREATHING
-	#define ENABLE_RGB_MATRIX_HUE_PENDULUM
-	#define ENABLE_RGB_MATRIX_HUE_WAVE
-	#define ENABLE_RGB_MATRIX_PIXEL_RAIN
-	#define ENABLE_RGB_MATRIX_PIXEL_FLOW
-	#define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
-	#define ENABLE_RGB_MATRIX_TYPING_HEATMAP
-	#define ENABLE_RGB_MATRIX_DIGITAL_RAIN
-	#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-	#define ENABLE_RGB_MATRIX_SOLID_REACTIVE
-	#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-	#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-	#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-	#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-	#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-	#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-	#define ENABLE_RGB_MATRIX_SPLASH
-	#define ENABLE_RGB_MATRIX_MULTISPLASH
-	#define ENABLE_RGB_MATRIX_SOLID_SPLASH
-	#define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+    // #define RGB_MATRIX_TIMEOUT 0 // number of milliseconds to wait until rgb automatically turns off
+    // #define RGB_MATRIX_SLEEP // turn off effects when suspended
+    #define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+    #define RGB_MATRIX_LED_FLUSH_LIMIT 10 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+    #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120 // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+    // #define RGB_MATRIX_DEFAULT_ON true // Sets the default enabled state, if none has been set
+    // #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT // Sets the default mode, if none has been set
+    // #define RGB_MATRIX_DEFAULT_HUE 0 // Sets the default hue value, if none has been set
+    // #define RGB_MATRIX_DEFAULT_SAT 255 // Sets the default saturation value, if none has been set
+    // #define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
+    #define RGB_MATRIX_DEFAULT_SPD 100 // Sets the default animation speed, if none has been set
+    #define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 50 // In order to change the delay of temperature decrease
+    #define RGB_MATRIX_TYPING_HEATMAP_SPREAD 40 // Limit the distance the effect spreads to surrounding keys.
+    #define RGB_MATRIX_TYPING_HEATMAP_AREA_LIMIT 16 // Limit how hot surrounding keys get from each press.
+    #define RGB_MATRIX_TYPING_HEATMAP_INCREASE_STEP 32 // Decreasing this value increases the number of keystrokes needed to fully heat up the key.
+    #define RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE // Solid reactive effects will pulse RGB light on key presses with user configurable hues. To enable gradient mode that will automatically change reactive color, add the following define:
+    #define ENABLE_RGB_MATRIX_SOLID_COLOR
+    #define ENABLE_RGB_MATRIX_ALPHAS_MODS
+    // #define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+    // #define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+    #define ENABLE_RGB_MATRIX_BREATHING
+    // #define ENABLE_RGB_MATRIX_CYCLE_ALL
+    // #define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+    // #define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+    #define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+    // #define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+    // #define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+    #define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+    #define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+    #define ENABLE_RGB_MATRIX_DUAL_BEACON
+    #define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+    #define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+    #define ENABLE_RGB_MATRIX_RAINDROPS
+    #define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+    #define ENABLE_RGB_MATRIX_HUE_BREATHING
+    #define ENABLE_RGB_MATRIX_HUE_PENDULUM
+    #define ENABLE_RGB_MATRIX_HUE_WAVE
+    #define ENABLE_RGB_MATRIX_PIXEL_RAIN
+    #define ENABLE_RGB_MATRIX_PIXEL_FLOW
+    #define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+    #define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+    #define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+    #define ENABLE_RGB_MATRIX_SPLASH
+    #define ENABLE_RGB_MATRIX_MULTISPLASH
+    #define ENABLE_RGB_MATRIX_SOLID_SPLASH
+    #define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
     #define RGB_MATRIX_EFFECT_PIXEL_RAIN
-	#define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+    #define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
 
     // Define LED configuration here
     #define LED_CONFIG                                                                 \
@@ -104,14 +104,14 @@
             {0, 30}, {10, 30}, {20, 30}, {30, 30}, {40, 30}, {50, 30}, {60, 30},        \
             {70, 30}, {80, 30}, {90, 30}, {100, 30}, {110, 30}, {120, 30}, {130, 30},   \
             {140, 30}, {150, 30}, {160, 30}, {170, 30}                                  \
-         },                                                                             \
+        },                                                                             \
         {                                                                              \
             1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,                   \
             1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,                   \
             1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,                   \
-		    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 				   \
             1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,                   \
-			1, 1, 1, 1, 1, 1, 1, 1, 1                     							   \
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,                   \
+            1, 1, 1, 1, 1, 1, 1, 1, 1                                                   \
         }                                                                              \
     }
 #endif
