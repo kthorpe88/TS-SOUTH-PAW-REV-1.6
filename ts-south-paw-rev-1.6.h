eec8a113d0f6cf68ffe8c1a1831783b14d9a196f
TS Design Works <64564678+kthorpe88@users.noreply.github.com>
2025-02-28 21:54:03 -0500

    Update ts-south-paw-rev-1.6.h

--- ts-south-paw-rev-1.6.h
+++ ts-south-paw-rev-1.6.h
@@ -1,4 +1,4 @@
-// Copyright 2024 TDC
+// Copyright 2025 TS Desing Works LLC
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
